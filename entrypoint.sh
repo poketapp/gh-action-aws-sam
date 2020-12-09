6f70c4ce1fa4bf02bb9b7e842f4ecd25e66c0935
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2020-12-09 13:21:15 -0500

    Some fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,8 +62,8 @@ fi
 export PATH=$HOME/.local/bin:$PATH
 
 if [ -n "$AWS_LOCAL_START_LAMBDA" ]; then
-    sam build $DEBUG_MODE
-    sam local start-lambda --force-image-build $DEBUG_MODE &
+    sam build --use-container $DEBUG_MODE
+    sam local start-lambda $DEBUG_MODE &
     python3 -m pytest $PYTHON_TEST_DIR -v
 else
     sam build $DEBUG_MODE
