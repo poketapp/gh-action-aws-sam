d71592ba653d25be8f5b16c3b1c3bbbb89e5bf30
NabaSadiaSiddiqui <naba.sadia.siddiqui@gmail.com>
2020-12-09 15:37:04 -0500

    Connect to host

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,8 +63,7 @@ export PATH=$HOME/.local/bin:$PATH
 
 if [ -n "$INTEGRATION_TEST_MODE" ]; then
     sam build $DEBUG_MODE
-    sam local start-lambda --host '0.0.0.0' $DEBUG_MODE &
-    python3 -m pytest $PYTHON_TEST_DIR -v
+    sam local start-lambda --docker-network host $DEBUG_MODE &
 else
     sam build $DEBUG_MODE
     sam package --output-template-file packaged.yaml --s3-bucket $AWS_DEPLOY_BUCKET
